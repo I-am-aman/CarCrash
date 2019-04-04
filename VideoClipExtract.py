98b95e76156cc62e7db8474ab34b41653fb0dbc6
Jitendra Choudhary <34153486+88Jitendra@users.noreply.github.com>
2019-04-05 00:10:03 +0530

    Update VideoClipExtract.py

--- VideoClipExtract.py
+++ VideoClipExtract.py
@@ -1,4 +1,4 @@
 import subprocess
-
+import os
 subprocess.run(["scenedetect", "-i","/home/aman/Desktop/Mini-Project/videoplayback.mp4", "-o", "/home/aman/Desktop//Mini-Project/VideoClips", "detect-content", "-t", "27",
                 "split-video"])
