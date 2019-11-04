42b6211d8f58d8b5384cddf1b2f8435f4d1b8769
I-am-aman <readyaman@gmail.com>
2019-11-05 00:12:11 +0530

    K-means clustering added

--- GaborFeatureExtraction.py
+++ GaborFeatureExtraction.py
@@ -1,7 +1,10 @@
 import cv2
 import numpy as np
 import glob
+import matplotlib.pyplot as plt
 import os
+from sklearn.cluster import KMeans
+from collections import Counter, defaultdict
 
 
 # define gabor filter bank with different orientations and at different scales
@@ -14,6 +17,7 @@ def build_filters():
             kern = cv2.getGaborKernel((ksize, ksize), 1.0, theta, nu, 0.5, 0, ktype=cv2.CV_32F)
             kern /= 1.5 * kern.sum()
             gaborfilters.append(kern)
+    # print(gaborfilters)
     # print(len(gaborfilters))
     return gaborfilters
 
@@ -72,4 +76,24 @@ if __name__ == '__main__':
 
     print(allKeyFramesFeat)
     print(len(allKeyFramesFeat))
+
+    kmeans = KMeans(n_clusters=5, random_state=0).fit(allKeyFramesFeat)
+    print(kmeans.labels_)
+    #print(kmeans.cluster_centers_)
+
+    vectorForVideo = []
+    for eachCentroid in kmeans.cluster_centers_:
+        vectorForVideo.extend(eachCentroid)
+
+    counter = Counter(kmeans.labels_)
+    sorted(counter.items())
+    print(counter)
+    for i in range(5):
+        vectorForVideo.append(counter[i])
+
+    print(vectorForVideo)
+    print(len(vectorForVideo))
+
+    # plt.scatter(allKeyFramesFeat[:, 0], allKeyFramesFeat[:, 1], c=kmeans.labels_, cmap='rainbow')
+    # plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], color='black')
 # feat matrix is the feature vector for the image
