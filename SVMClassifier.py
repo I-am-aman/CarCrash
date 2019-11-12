1bedd452b99cdba5240f4de0e9395bab7dcf4e3a
I-am-aman <readyaman@gmail.com>
2019-11-12 22:54:02 +0530

    Scaling added in SVM Classifier

--- SVMClassifier.py
+++ SVMClassifier.py
@@ -1,19 +1,17 @@
-import pandas as pd
-import numpy as np
 import csv
-import random
-import matplotlib.pyplot as plt
-from sklearn.metrics import  accuracy_score
+# import random
+from sklearn.metrics import accuracy_score
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
+from sklearn import preprocessing
+from sklearn.preprocessing import MinMaxScaler
 
 with open("feature_vector.csv", 'r') as my_file:
     reader = csv.reader(my_file, delimiter=' ')
     feature_vector = list(reader)
 
 X = [list(map(float, lst)) for lst in feature_vector]
-print(X)
 
 with open("label_vector.csv", 'r') as my_file:
     reader = csv.reader(my_file, delimiter=' ')
@@ -21,13 +19,14 @@ with open("label_vector.csv", 'r') as my_file:
 
 y_temp_list = [list(map(int, lst)) for lst in label_vector]
 y = [j for sub in y_temp_list for j in sub]
-print(y)
 
-shuffle_vector = list(zip(X, y))
-random.shuffle(shuffle_vector)
-X, y = zip(*shuffle_vector)
+X_scaled = preprocessing.scale(X)
+print(X_scaled)
+scaler = MinMaxScaler(feature_range=(0, 1))
+rescaledX = scaler.fit_transform(X)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+X_train, X_test, y_train, y_test = train_test_split(rescaledX, y, test_size=0.20)
+
 svclassifier = SVC(kernel='linear')
 svclassifier.fit(X_train, y_train)
 y_pred = svclassifier.predict(X_test)
